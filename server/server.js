c0048718df447b075498d4b8c38bc2b40eaccfd5
leonado1204 <leonado124@outlook.com>
2022-08-25 00:24:28 -0700

    MongoDB

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.path.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
+const dbo = require("./db/conn");
+
+app.listen(port, () => {
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+  });
+  console.log(`Server is running on ${port}`);
+});
